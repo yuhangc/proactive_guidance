95192ea0da231c073d01315b58f09f5f26c30185
Yuhang <cccumg@gmail.com>
2018-09-26 09:16:52 -0700

    recomputed some models and policies

--- scripts/model_plan/gp_model_approx.py
+++ scripts/model_plan/gp_model_approx.py
@@ -45,7 +45,7 @@ class GPModelApproxBase(object):
             #                                                          n_restarts_optimizer=3))
             gp_mean.append(gaussian_process.GaussianProcessRegressor(kernel=1.0 * RBF(length_scale=1.0) +
                                                                             0.1 *
-                                                                            WhiteKernel(0.1, noise_level_bounds=(0.01, 0.2)),
+                                                                            WhiteKernel(0.1, noise_level_bounds=(0.01, 0.25)),
                                                                      n_restarts_optimizer=3))
 
             # gp_std.append(gaussian_process.GaussianProcessRegressor(kernel=1.0 * RBF(length_scale=0.3) +
